fb794c71adcf4bb3cb7d6412f9962dfcb4ad1a57
Marlen13 <olenamarinova@gmail.com>
2023-07-18 17:08:55 +0200

    delete previous code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  };
+  render() {
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <form >
+                  <label htmlFor="">
+                    <input type="text" />
+                  </label>
+                  <button type="submit"></button>
+                </form>  
+      </>
+    )
+  }
+  // return (
+  //   <div
+  //     style={{
+  //       height: '100vh',
+  //       display: 'flex',
+  //       justifyContent: 'center',
+  //       alignItems: 'center',
+  //       fontSize: 40,
+  //       color: '#010101'
+  //     }}
+  //   >
+  //     React homework template
+  //   </div>
+  // );
 };
